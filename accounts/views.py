b6ac35a200c7fdfceaa08e0dec153f4e25304673
unknown <viet.dinh217@gmail.com>
2020-11-20 13:25:16 +0700

    update query ra dashboard và product

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'customers': customers, 'orders': orders, 'total_orders': total_orders,
+        'delivered':delivered , 'pending':pending
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customers(request):
     return render(request, 'accounts/customers.html')
