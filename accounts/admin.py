d58f80e5646fc68e1f60d84bac531c2a3f57c33c
unknown <viet.dinh217@gmail.com>
2020-11-20 12:36:40 +0700

    update table and query demo

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,14 +2,29 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
-
+#chỉnh lại hiển thị Custemer
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['name','phone','email','date_created']
     list_filter = ['date_created']
     search_fields = ['name']
 
+#chỉnh lại hiển thị Order    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['custumer','product','status']
+    list_filter = ['date_created']
+    search_fields = ['custumer']
+
+#chỉnh lại hiển thị product
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','category']
+    list_filter = ['date_created']
+    search_fields = ['name']
+
+#đăng ký hiện lên admin    
 admin.site.register(Customer, CustomerAdmin)
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
+
+admin.site.register(Tag)
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
 '''tạo bảng database custumer'''
@@ -7,6 +8,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+#tạo bảng tag
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
@@ -19,8 +26,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices = CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 '''tạo bảng order'''
 class Order(models.Model):
@@ -29,8 +40,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    #custumer =
-    # product = 
+    custumer = models.ForeignKey(Customer, null=True , on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null=True , on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices= STATUS)
 
